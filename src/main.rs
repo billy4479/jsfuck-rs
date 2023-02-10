7aa442bb774c28e494520ac5dc046beae07ef81b
billy4479 <giachi.ellero@gmail.com>
2023-02-10 08:56:47 +0100

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,158 @@
+use std::collections::HashMap;
+
+struct Compiler {
+    map: HashMap<char, String>,
+}
+
+impl Compiler {
+    fn number(n: usize) -> String {
+        let zero = "+[]";
+        let one = "+!![]";
+
+        if n == 0 {
+            zero.to_owned()
+        } else {
+            let v = vec![one; n];
+            v.join("+")
+        }
+    }
+
+    fn from_string(&self, s: impl Into<String>) -> String {
+        let s: String = s.into();
+
+        s.chars()
+            .map(|z| {
+                if !self.map.contains_key(&z) {
+                    // FIXME: we assume we need just one u16, we just panic otherwise
+                    let mut b = [0; 1];
+                    z.encode_utf16(&mut b);
+
+                    format!(
+                        "([]+[])[{}][{}]({})",
+                        self.from_string("constructor"),
+                        self.from_string("fromCharCode"),
+                        Self::number(b[0].into())
+                    )
+                } else {
+                    self.map.get(&z).expect("unreachable").to_owned()
+                }
+            })
+            .collect::<Vec<String>>()
+            .join("+")
+    }
+
+    fn populate_map(&mut self) {
+        self.map
+            .insert('a', format!("(+{{}}+[])[{}]", Self::number(1)));
+        self.map
+            .insert('b', format!("({{}}+[])[{}]", Self::number(2)));
+        self.map
+            .insert('o', format!("({{}}+[])[{}]", Self::number(1)));
+        self.map
+            .insert('e', format!("({{}}+[])[{}]", Self::number(4)));
+        self.map
+            .insert('c', format!("({{}}+[])[{}]", Self::number(5)));
+        self.map
+            .insert('t', format!("({{}}+[])[{}]", Self::number(6)));
+        self.map
+            .insert(' ', format!("({{}}+[])[{}]", Self::number(7)));
+        self.map
+            .insert('f', format!("(![]+[])[{}]", Self::number(0)));
+        self.map
+            .insert('s', format!("(![]+[])[{}]", Self::number(3)));
+        self.map
+            .insert('r', format!("(!![]+[])[{}]", Self::number(1)));
+        self.map
+            .insert('u', format!("(!![]+[])[{}]", Self::number(2)));
+        self.map
+            .insert('i', format!("((+!![]/+[])+[])[{}]", Self::number(3)));
+        self.map
+            .insert('n', format!("((+!![]/+[])+[])[{}]", Self::number(4)));
+        self.map.insert(
+            'S',
+            format!(
+                "([]+([]+[])[{}])[{}]",
+                self.from_string("constructor"),
+                Self::number(9)
+            ),
+        );
+        self.map.insert(
+            'g',
+            format!(
+                "([]+([]+[])[{}])[{}]",
+                self.from_string("constructor"),
+                Self::number(14)
+            ),
+        );
+        self.map
+            .insert('\\', format!("(/\\\\/+[])[{}]", Self::number(1)));
+        self.map.insert(
+            'p',
+            format!(
+                "([]+(/-/)[{}])[{}]",
+                self.from_string("constructor"),
+                Self::number(14)
+            ),
+        );
+        self.map.insert(
+            'd',
+            format!(
+                "({})[{}]({})",
+                Self::number(13),
+                self.from_string("toString"),
+                Self::number(14)
+            ),
+        );
+        self.map.insert(
+            'h',
+            format!(
+                "({})[{}]({})",
+                Self::number(17),
+                self.from_string("toString"),
+                Self::number(18)
+            ),
+        );
+        self.map.insert(
+            'm',
+            format!(
+                "({})[{}]({})",
+                Self::number(22),
+                self.from_string("toString"),
+                Self::number(23)
+            ),
+        );
+        self.map.insert(
+            'C',
+            format!(
+                "((()=>{{}})[{}]({})()({}))[{}]",
+                self.from_string("constructor"),
+                self.from_string("return escape"),
+                self.map.get(&'\\').expect("unreachable"),
+                Self::number(2)
+            ),
+        );
+    }
+
+    pub fn compile(&self, code: impl Into<String>) -> String {
+        format!(
+            "(()=>{{}})[{}]({})()",
+            self.from_string("constructor"),
+            self.from_string(code)
+        )
+    }
+
+    pub fn new() -> Self {
+        let mut result = Self {
+            map: HashMap::new(),
+        };
+
+        result.populate_map();
+        result
+    }
+}
+
+fn main() {
+    let compiler = Compiler::new();
+
+    println!("{}", compiler.compile("console.log(\"hello world\");"));
+}
